20179f67afbfb0e91a7fde943d6671cd0016ec02
vikasmathod <vikasmathodu@gmail.com>
2014-09-26 22:17:02 +0530

    Code refactored

--- test/commands/cli.vm.create_win_rdp-tests.js
+++ test/commands/cli.vm.create_win_rdp-tests.js
@@ -144,22 +144,18 @@ describe('cli', function() {
 
     // Get name of an image of the given category
     function getImageName(category, callBack) {
-      if (process.env.VM_WIN_IMAGE) {
-        callBack(process.env.VM_WIN_IMAGE);
-      } else {
-        var cmd = util.format('vm image list --json').split(' ');
-        testUtils.executeCommand(suite, retry, cmd, function(result) {
-          result.exitStatus.should.equal(0);
-          var imageList = JSON.parse(result.text);
-          imageList.some(function(image) {
-            if ((image.operatingSystemType || image.oSDiskConfiguration.operatingSystem).toLowerCase() === category.toLowerCase() && image.category.toLowerCase() === 'public') {
-              process.env.VM_WIN_IMAGE = image.name;
-              return true;
-            }
-          });
-          callBack(process.env.VM_WIN_IMAGE);
+      var cmd = util.format('vm image list --json').split(' ');
+      testUtils.executeCommand(suite, retry, cmd, function(result) {
+        result.exitStatus.should.equal(0);
+        var imageList = JSON.parse(result.text);
+        imageList.some(function(image) {
+          if ((image.operatingSystemType || image.oSDiskConfiguration.operatingSystem).toLowerCase() === category.toLowerCase() && image.category.toLowerCase() === 'public') {
+            vmImgName = image.name;
+            return true;
+          }
         });
-      }
+        callBack(vmImgName);
+      });
     }
 
     function createReservedIp(location, callback) {
